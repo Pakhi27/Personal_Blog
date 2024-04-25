7958329eee65d2c60fbba386f57b6e782bb0633c
Pakhi27 <137711794+Pakhi27@users.noreply.github.com>
2024-04-25 18:41:09 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    document.getElementById("element").textContent = "Dancer";
+}, 2000); // Change to "Dancer" after 2 seconds
+
+setTimeout(function() {
+    document.getElementById("element").textContent = "Singer";
+}, 4000); // Change to "Singer" after 4 seconds
